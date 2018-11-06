3d0d32347a02d78b0f22dec0e3441cc9f586d797
Derek Crew <crewderek@yahoo.com>
2018-11-06 16:59:59 -0700

    Website for Best Resting Inn

---
+++ SUBS/header.php
@@ -0,0 +1,12 @@
+<nav>
+	<a id="header-home-logo" href="/"><img src="/images/best-resting.jpeg"></a>
+	
+	<button id="mobile-nav" ><img src="http://www.widerfunnel.com/wp-content/themes/Avada/images/icon-mobile-nav.png"></button>
+	<ul id="website-navigation">
+		<li><a class="nav-highlight" href="#" onclick="ChangeContent('home-content')">Home</a></li>
+		<li><a class="nav-highlight" href="#" onclick="ChangeContent('room-rates-content')">Room Rates</a></li>
+<!--		<li><a class="nav-highlight" href="#" onclick="ChangeContent('contact-us-content')">Contact Us</a></li>
+-->		<li><a class="nav-highlight" href="#" onclick="ChangeContent('about-us-content')">About Us</a></li>
+		<li><a class="nav-highlight" href="#" onclick="ChangeContent('photos-content')">Photos</a></li>
+	</ul>
+</nav>
