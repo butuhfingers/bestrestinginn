80c96bf514d74c125615ff5f11f2facf767d07af
Derek Crew <crewderek@yahoo.com>
2018-11-06 17:32:59 -0700

    Update image viewer

--- SUBS/header.php
+++ SUBS/header.php
@@ -1,7 +1,7 @@
 <nav>
 	<a id="header-home-logo" href="/"><img src="/images/best-resting.jpeg"></a>
 	
-	<button id="mobile-nav" ><img src="http://www.widerfunnel.com/wp-content/themes/Avada/images/icon-mobile-nav.png"></button>
+	<button id="mobile-nav" ><img src="http://www.stickpng.com/assets/images/588a64f5d06f6719692a2d13.png"></button>
 	<ul id="website-navigation">
 		<li><a class="nav-highlight" href="#" onclick="ChangeContent('home-content')">Home</a></li>
 		<li><a class="nav-highlight" href="#" onclick="ChangeContent('room-rates-content')">Room Rates</a></li>
